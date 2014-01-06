6e8ca8531055e3747bea7817ab747d63f984d33f
Chris Talkington <chris@talkingtontech.com>
2014-01-05 19:05:25 -0600

    init test file.

---
+++ test/pack.js
@@ -0,0 +1,11 @@
+/*global before,describe,it */
+var assert = require('chai').assert;
+var mkdir = require('mkdirp');
+
+describe('pack', function() {
+
+  it.skip('should do something at some point', function() {
+
+  });
+
+});
